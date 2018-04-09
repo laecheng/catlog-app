5ee91cfd382ae24f7e1884353988939f4977d8b9
laecheng <chc286@ucsd.edu>
2018-04-08 17:31:48 -0700

    feat: added local permission to the web application

--- application.py
+++ application.py
@@ -31,15 +31,22 @@ session = DBSession()
 # Create anti-forgery state token
 @app.route('/login')
 def showLogin():
+    """
+    Generate a ramdom token will be used to provent anti-forgery STATE
+    attack and render the login template
+    """
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """
+    Gathers data from Google Sign In API and places it inside
+    a session variable that will be used to better serve the user.
+    """
     # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -128,6 +135,7 @@ def gconnect():
 
 # User Helper Functions
 def createUser(login_session):
+    """Helper method for gconnect, create a new user to database"""
     newUser = User(name=login_session['username'],
                    email=login_session['email'])
     session.add(newUser)
@@ -137,11 +145,13 @@ def createUser(login_session):
 
 
 def getUserInfo(user_id):
+    """Helper method for gconnect, get user info from database"""
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
 def getUserID(email):
+    """Helper method for gconnect, get user info from database"""
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
@@ -151,6 +161,9 @@ def getUserID(email):
 
 @app.route('/gdisconnect')
 def gdisconnect():
+    """
+    Disconnect user from google plus sign in and clean the login session
+    """
     # Only disconnect a connected user.
     access_token = login_session.get('access_token')
     if access_token is None:
@@ -175,6 +188,7 @@ def gdisconnect():
 @app.route('/')
 @app.route('/catlog')
 def showCatlogs():
+    """Render the catlog page using the catlogs.html template"""
     catlogs = session.query(Catlog).order_by(asc(Catlog.name))
     return render_template('catlogs.html',
                            catlogs=catlogs,
@@ -184,6 +198,7 @@ def showCatlogs():
 @app.route('/catlog/<int:catlog_id>/')
 @app.route('/catlog/<int:catlog_id>/item/')
 def showItem(catlog_id):
+    """Render the item page using the item.html template"""
     catlog = session.query(Catlog).filter_by(id=catlog_id).one()
     items = session.query(CatlogItem).filter_by(catlog_id=catlog_id).all()
     return render_template('item.html', items=items, catlog=catlog)
@@ -191,6 +206,7 @@ def showItem(catlog_id):
 
 @app.route('/catlog/<int:catlog_id>/<int:item_id>/')
 def showItemInfo(catlog_id, item_id):
+    """Render the item info page using the itemDescription.html template"""
     catlog = session.query(Catlog).filter_by(id=catlog_id).one()
     item = session.query(CatlogItem).filter_by(id=item_id).one()
     return render_template('itemDescription.html', item=item, catlog=catlog)
@@ -198,13 +214,19 @@ def showItemInfo(catlog_id, item_id):
 
 @app.route('/catlog/<int:catlog_id>/item/new/', methods=['GET', 'POST'])
 def newItem(catlog_id):
+    """
+    function receive form data from user and create a new item to
+    store this item in the database.
+    """
     if 'username' not in login_session:
-        return redirect('/login')
+        flash('user must be login to modify the item')
+        return redirect(url_for('showCatlogs'))
     catlog = session.query(Catlog).filter_by(id=catlog_id).one()
     if request.method == 'POST':
         newItem = CatlogItem(name=request.form['name'],
                              description=request.form['description'],
-                             catlog_id=catlog_id)
+                             catlog_id=catlog_id,
+                             user_id=login_session['user_id'])
         session.add(newItem)
         session.commit()
         flash('New %s Item Successfully Created' % (newItem.name))
@@ -216,10 +238,22 @@ def newItem(catlog_id):
 @app.route('/catlog/<int:catlog_id>/<int:item_id>/edit',
            methods=['GET', 'POST'])
 def editItem(catlog_id, item_id):
-    if 'username' not in login_session:
-        return redirect('/login')
+    """
+    function receive form data from user and edit the item and update the
+    item in the database
+    """
     catlog = session.query(Catlog).filter_by(id=catlog_id).one()
     editItem = session.query(CatlogItem).filter_by(id=item_id).one()
+    if 'username' not in login_session:
+        flash('user must be login to modify the item')
+        return redirect(url_for('showCatlogs'))
+    if editItem.user_id != login_session['user_id']:
+        print "editItem.user_id: " + str(editItem.user_id)
+        print "login_session['user_id']: " + str(login_session['user_id'])
+        flash('You are not authorized to edit this restaurant' +
+              'Please create your own restaurant in order to edit')
+        return redirect(url_for('showItem', catlog_id=catlog_id))
+
     if request.method == 'POST':
         if request.form['name']:
             editItem.name = request.form['name']
@@ -239,8 +273,13 @@ def editItem(catlog_id, item_id):
 @app.route('/catlog/<int:catlog_id>/<int:item_id>/delete',
            methods=['GET', 'POST'])
 def deleteItem(catlog_id, item_id):
+    """
+    function receive form data from user and delete the item and delete the
+    item in the database
+    """
     if 'username' not in login_session:
-        return redirect('/login')
+        flash('user must be login to modify the item')
+        return redirect(url_for('showCatlogs'))
     catlog = session.query(Catlog).filter_by(id=catlog_id).one()
     try:
         itemToDelete = session.query(CatlogItem).filter_by(id=item_id).one()
